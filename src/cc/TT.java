19754346d655cc0791f071e7a889cf90901a3c68
chencheng <cchzxy@126.com>
2017-02-13 16:18:58 +0800

    11

--- src/cc/TT.java
+++ src/cc/TT.java
@@ -1,8 +1,11 @@
 package cc;
 
+import java.util.Date;
+
 public class TT {
 	public static void main(String[] args){
 		System.out.println("111");
 		System.out.println("222");
+		Date d = new Date();
 	}
 }
